7a4122a041a574b58cadc9485d8efbd0340ee558
robbyvan <fzpinus@gmail.com>
2016-10-27 13:53:41 -0700

    4+200+load error fixed
    Caused by dataType. JSONP -> JSON

--- twitch.js
+++ twitch.js
@@ -1,4 +1,14 @@
 $(document).ready(function (){
+  var channels = ["freecodecamp", "riotgames", "blizzard", "medrybw", "dota2ti"];
+
+  channels.forEach(function(channel){
+    var channelInfo = getChannelInfo(channel);
+    console.log(channelInfo);
+  });
+
+});
+
+function getChannelInfo(channel){
   $.ajax({
     headers: {
       Accept: "application/vnd.twitchtv.v3+json"
@@ -13,20 +23,23 @@ $(document).ready(function (){
       No idea why this happens.
       If add "?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23" to the URL, 
       then it works. Maybe figure this out later*/
-      
+
     },
-    url: "https://api.twitch.tv/kraken/channels/freecodecamp?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23",
+    url: "https://api.twitch.tv/kraken/channels/" + channel +"?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23",
     type: "GET",
-    dataType: "jsonp",
-    jsonp: "callback",
-    jsonpCallback: "callbackFunction",
+    dataType: "json",
+    // jsonp: "callback",
+    // jsonpCallback: "callbackFunction",
+    /*jsonp is not always the correct choice, here if using jsonp will get 4+200+load error*/
     success: function(r){
       console.log("ajax success");
       console.log(r);
+      return r;
     },
     error: function(e){
       console.log("ajax failed");
       console.log(e);
+      return false;
     }
   });
-});
+}
