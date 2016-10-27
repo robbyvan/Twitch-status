d600221daa2f021443401b30e86bb9d8d6e0e430
robbyvan <fzpinus@gmail.com>
2016-10-27 14:29:48 -0700

    "asynchronous " problem
    cuz used AJAX, the time when userInfo & streamInfo arrived are not
    ordered like
        in loop here, i.e, not in pairs: user1 -> streamInfo1 -> user2 ->
    streamInfo2 ->
        Thats's why the output in console are out of order(however
    respectively in order .)
        So need figure out how to match them

--- twitch.js
+++ twitch.js
@@ -2,15 +2,41 @@ $(document).ready(function (){
   var channels = ["freecodecamp", "riotgames", "blizzard", "medrybw", "dota2ti"];
 
   channels.forEach(function(channel){
+    getUserInfo(channel);
     getChannelInfo(channel);
+
+    /*'cuz used AJAX, the time when userInfo & streamInfo arrived are not ordered like 
+    in loop here, i.e, not in pairs: user1 -> streamInfo1 -> user2 -> streamInfo2 ->
+    Thats's why the output in console are out of order(however respectively in order .)
+    So need figure out how to match them */
+
     // console.log(channelInfo);
     /*should not process data in another function, here it will print undefined
     Process the data in $.ajax().success() */
   });
 });
 
-function displayChannels(channelInfo){
-  
+function displayChannels(streamInfo){
+
+}
+function getUserInfo(channel){
+  $.ajax({
+    headers: {
+      Accept: "application/vnd.twitchtv.v3+json"
+    },
+    url: "https://api.twitch.tv/kraken/users/" + channel +"?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23",
+    type: "GET",
+    dataType: "json",
+    success: function(response){
+      console.log("ajax success");
+      console.log(response);
+
+    },
+    error: function(error){
+      console.log("ajax failed");
+      console.log(error);
+    }
+  });
 }
 
 function getChannelInfo(channel){
