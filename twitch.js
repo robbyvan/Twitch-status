0f7d0b106dbd41910d738af667d6e9eabd207e5a
robbyvan <fzpinus@gmail.com>
2016-10-27 13:23:28 -0700

    Status 400 fixed
    Still not very clear why I added client id in the header, server keeps
    telling me client id not specified.

--- twitch.js
+++ twitch.js
@@ -1,9 +1,21 @@
 $(document).ready(function (){
   $.ajax({
     headers: {
-      Accept: "application/vnd.twitchtv.v3+json",
+      Accept: "application/vnd.twitchtv.v3+json"
+      //,"Client-ID": "<cqvrrm8zniwpbvkyxxl036y927jlm23>"
+      //"Client-ID": "cqvrrm8zniwpbvkyxxl036y927jlm23"
+      //"client_id": "<cqvrrm8zniwpbvkyxxl036y927jlm23>"
+
+      /*Somehow all commented lines above does not work, 
+      though Twitch says include client id in the header should be like this: 
+            Client-ID: <client_id>
+      But I still get 400 error (Bad request) says "No client id specified" 
+      No idea why this happens.
+      If add "?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23" to the URL, 
+      then it works. Maybe figure this out later*/
+      
     },
-    url: "https://api.twitch.tv/kraken/streams/test_channel",
+    url: "https://api.twitch.tv/kraken/channels/freecodecamp?client_id=cqvrrm8zniwpbvkyxxl036y927jlm23",
     type: "GET",
     dataType: "jsonp",
     jsonp: "callback",
